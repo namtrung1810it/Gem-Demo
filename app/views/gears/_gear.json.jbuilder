1ddb5a0f006366f13e236dc8d41e404c5d17d416
TrungBN <namtrung18101996@gmail.com>
2019-03-03 11:52:18 +0700

    First commit

---
+++ app/views/gears/_gear.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! gear, :id, :name, :price, :created_at, :updated_at
+json.url gear_url(gear, format: :json)
---
+++ db/migrate/20190303040259_create_gears.rb
@@ -0,0 +1,10 @@
+class CreateGears < ActiveRecord::Migration[5.2]
+  def change
+    create_table :gears do |t|
+      t.string :name
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
